82a015bbc50f102f28449e58131fbead46526c02
Leeon <leeon.ewubd@gmail.com>
2024-01-08 00:12:38 +0600

    admin make & jwt install

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+var jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 //middleware
 app.use(cors())
 app.use(express.json());
 
+//2cb07e194c50b4528985019cb51c7b9f4a0d5f770c2f4f29a3353edf0709610d1dccba5a95c26137a6aea39d5a865985ee5784e03c3561fbb345ac00a73bdf76
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jfba5ry.mongodb.net/?retryWrites=true&w=majority`;
@@ -32,18 +34,38 @@ async function run() {
 
     // user related api
 
-    app.get('/users', async(req,res)=>{
+    app.get('/users', async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     })
     app.post('/users', async (req, res) => {
       const user = req.body;
-      const query ={email:user.email}
+      const query = { email: user.email }
       const existingUser = await userCollection.findOne(query);
-      if(existingUser){
-        return res.send({message:'user already exists',insertedId:null})
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
       }
       const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     })
 
